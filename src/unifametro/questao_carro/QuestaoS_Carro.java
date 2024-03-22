16562903f193892bd62e6d027d8a40ca5e864f08
Guilherme <luizguilhermeinformat@gmail.com>
2024-03-22 00:43:23 -0300

    feat: new function for checking mileage and liters #3

--- src/unifametro/questao_carro/QuestaoS_Carro.java
+++ src/unifametro/questao_carro/QuestaoS_Carro.java
@@ -2,9 +2,16 @@ package src.unifametro.questao_carro;
 
 public class QuestaoS_Carro {
     public static void main(String[] args) {
-    QuestaoC_Carro ferrari = new QuestaoC_Carro(5);
-    ferrari.adicionarGasolina(50);
-    ferrari.andar(10);
-    System.out.println("Tanque Ferrari: " + ferrari.obterGasolina() + " litros");
+        // Cria uma instância da classe QuestaoC_Carro com um consumo de 5 km/l
+        QuestaoC_Carro ferrari = new QuestaoC_Carro(5);
+
+        // Abastece o tanque da Ferrari com 50 litros de gasolina
+        ferrari.adicionarGasolina(50);
+
+        // Simula a condução da Ferrari por 10 km
+        ferrari.andar(10);
+
+        // Exibe o nível atual de gasolina no tanque da Ferrari
+        System.out.println("Tanque Ferrari: " + ferrari.obterGasolina() + " litros");
     }
 }
