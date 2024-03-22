e6cf10436bdbd368c5fe836544002aafaeb6db46
⟡ Sabrine M <140846441+SabrineSilva@users.noreply.github.com>
2024-03-22 13:16:53 -0300

    Teste webhook - segunda tentativa
    segunda tentativa do teste webhook:
    - adicionei um /github ao payload

--- src/unifametro/questao_somaZeroDez.java
+++ src/unifametro/questao_somaZeroDez.java
@@ -10,7 +10,7 @@ public class questao_somaZeroDez {
 
             resultado = resultado + i;
             System.out.println("resultado: " + resultado);
-        // teste de alteração sendo realizada
+        // Outro teste de alteração
 
         }
 
