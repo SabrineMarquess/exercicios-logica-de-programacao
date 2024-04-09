3c96abfbbbd285363aeb86d34478c52f4fd9c550
⟡ Sabrine M. <140846441+SabrineSilva@users.noreply.github.com>
2024-04-09 15:14:17 -0300

    Questão [21]: indentando + criação da classe executável #7

--- src/exercicios/questao_21/questao_media10Num_v2.java
+++ src/exercicios/questao_21/questao_media10Num_v2.java
@@ -1,4 +1,11 @@
 package src.exercicios.questao_21;
 
 public class questao_media10Num_v2 {
+    public static void main(String[] args) {
+        // Escreva um programa que calcule e imprima a média de 10 números introduzidos pelo usuário
+
+    }
 }
+// Escreva um programa que calcule e imprima a média de 10 números introduzidos pelo usuário
+
+
