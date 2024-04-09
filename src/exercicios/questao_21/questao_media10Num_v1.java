1ee0d5ac4cf532f7da3a2a91aaf9ff6e8f1dbed2
⟡ Sabrine M. <140846441+SabrineSilva@users.noreply.github.com>
2024-04-09 15:14:17 -0300

    Questão [21]: indentando + criação da classe executável #7

--- src/exercicios/questao_21/questao_media10Num_v1.java
+++ src/exercicios/questao_21/questao_media10Num_v1.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 
 public class questao_media10Num_v1 {
     public static void main(String[] args) {
-        //2. Escreva um programa que calcule e imprima a
-        //média de 10 números introduzidos pelo usuário
+        // Escreva um programa que calcule e imprima a média de 10 números introduzidos pelo usuário
 
         Scanner ler = new Scanner(System.in);
         int num = 0;
         double soma = 0, media;
-        for (int i=1; i<=10 ; i++){
-            System.out.println("Informe o Valor do "+i+" Número");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Informe o Valor do " + i + " Número");
             num = ler.nextInt();
             soma += num;
         }
-            media = soma /10;
-            System.out.println("Número: "+num+" soma: "+soma+" media:"+media);
+        media = soma / 10;
+        System.out.println("Número: " + num + " soma: " + soma + " media:" + media);
     }
 }
